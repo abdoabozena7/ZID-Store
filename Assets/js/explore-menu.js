81686424bc37c2ccb98b74ce0080bf5f1b044444
Abderahman Mohamed <145558673+abdoabozena7@users.noreply.github.com>
2026-01-23 02:25:38 +0200

    Update explore-menu.js

--- Assets/js/explore-menu.js
+++ Assets/js/explore-menu.js
@@ -1201,7 +1201,7 @@ function InfiniteMenu({ items = [], scale = 1.0 }) {
     loaderStyleInjected.current = true;
   }, []);
 
-  const loadingText = '\u0627\u0633\u062a\u0646\u0649 \u0634\u0648\u064a\u0629 \u0648\u0627\u062d\u0646\u0627 \u0628\u0646\u062c\u064a\u0628 \u0627\u0644\u0645\u0646\u062a\u062c\u0627\u062a';
+  const loadingText = 'استني اللبس بيحمل ';
 
   return React.createElement(
     'div',
